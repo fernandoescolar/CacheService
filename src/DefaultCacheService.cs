ad5478becabdef506a94e7b6459fdebadb0c1824
Fernando Escolar <f.escolar@hotmail.com>
2021-11-05 11:32:12 +0100

    adding InvalidateAsync method

--- src/DefaultCacheService.cs
+++ src/DefaultCacheService.cs
@@ -21,5 +21,17 @@ namespace CacheService
             var context = new ChainContext<T>(key, options, getter, cancellationToken);
             return HandleAsync(context);
         }
+
+        public Task InvalidateAsync(string key, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var options = new CacheServiceOptions();
+            options.Memory.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(0);
+            options.Distributed.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(0);
+
+            var context = new ChainContext<object>(key, options, ct => ValueTask.FromResult<object?>(default), cancellationToken);
+            return HandleAsync(context).AsTask();
+        }
     }
 }
